ba44c1f05c8ce0ae9a323408edbd1c7752251db2
Bisio98 <alberto.bisetti@hotmail.it>
2022-02-21 16:43:46 +0100

    first commit

---
+++ resources/js/views/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <WorkInProgress/>
+</template>
+
+<script>
+import WorkInProgress from '../components/WorkInProgress.vue'
+
+export default {
+  components: { WorkInProgress },
+    name: 'App'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
